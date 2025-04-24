832439c166abbd2f315b8abd47c44030ef428d40
iamdebajyotibanerjee <ips.qsf08@gmail.com>
2025-04-24 16:49:22 +0530

    Challenges bupto 2 completed

---
+++ 04_Object_challenge/script.js
@@ -0,0 +1,47 @@
+
+// Create the library object
+const library = [
+    {
+        title: "Golpo Guccha",
+        author: "Rabindranath Tagore",
+        status: {
+            own: true,
+            reading: false,
+            read: false
+        }
+    },
+    {
+        title: "Ghanada Samagra",
+        author: "Premendra Mitra",
+        status: {
+            own: true,
+            reading: false,
+            read: false
+        }
+    },
+    {
+        title: "Shanku Samagra",
+        author: "Satyajit Ray",
+        status: {
+            own: true,
+            reading: false,
+            read: false
+        }
+    }
+];
+
+console.log(`First challenge check : library = `);
+console.log(library);
+
+// Set the value of 'read' property to true for all the books. Do not use funstions, loops or higher order methods
+
+library[0].status.read = true;
+library[1].status.read = true;
+library[2].status.read = true;
+
+console.log(`Second challenge check : library = `);
+console.log(library);
+
+
+// Destructure the 'title' property from the the first book in the library and rename it to firstBook 
+
