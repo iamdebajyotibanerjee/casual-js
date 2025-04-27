4dcc601f6dcefb5a5dee5217471175466c57302b
Dev <ips.qsf08@gmail.com>
2025-04-27 22:51:42 +0530

    Third challenge completed

--- 04_Object_challenge/script.js
+++ 04_Object_challenge/script.js
@@ -1,33 +1,32 @@
-
 // Create the library object
 const library = [
-    {
-        title: "Golpo Guccha",
-        author: "Rabindranath Tagore",
-        status: {
-            own: true,
-            reading: false,
-            read: false
-        }
+  {
+    title: "Golpo Guccha",
+    author: "Rabindranath Tagore",
+    status: {
+      own: true,
+      reading: false,
+      read: false,
     },
-    {
-        title: "Ghanada Samagra",
-        author: "Premendra Mitra",
-        status: {
-            own: true,
-            reading: false,
-            read: false
-        }
+  },
+  {
+    title: "Ghanada Samagra",
+    author: "Premendra Mitra",
+    status: {
+      own: true,
+      reading: false,
+      read: false,
     },
-    {
-        title: "Shanku Samagra",
-        author: "Satyajit Ray",
-        status: {
-            own: true,
-            reading: false,
-            read: false
-        }
-    }
+  },
+  {
+    title: "Shanku Samagra",
+    author: "Satyajit Ray",
+    status: {
+      own: true,
+      reading: false,
+      read: false,
+    },
+  },
 ];
 
 console.log(`First challenge check : library = `);
@@ -42,6 +41,10 @@ library[2].status.read = true;
 console.log(`Second challenge check : library = `);
 console.log(library);
 
+// Destructure the 'title' property from the the first book in the library and rename it to firstBook
 
-// Destructure the 'title' property from the the first book in the library and rename it to firstBook 
+const { title: firstBook } = library[0];
+console.log(`Third challenge check : firstBook = `);
+console.log(firstBook);
 
+// Turn the library object into a JSON string
