338a020f8a89e3ff8c12e7bf5bb472ff0a5b90ce
Dev <ips.qsf08@gmail.com>
2025-05-02 07:55:01 +0530

    minmax challenge completed

--- 05_Function_challenge/script.js
+++ 05_Function_challenge/script.js
@@ -6,3 +6,26 @@ function getCelcius(F) {
   return console.log(`Celcius = ${celcius}`);
 }
 getCelcius(farenheit);
+
+// minMax function
+const numbers = [21, 36, 77, 878, 52, 95, 61, 327, 12, 45];
+let min = 0,
+  max = 0;
+const minMax = (arr) => {
+  min = arr[0];
+  max = arr[0];
+  arr.forEach((item) => {
+    if (item < min) {
+      min = item;
+    }
+    if (item > max) {
+      max = item;
+    }
+  });
+  return {
+    minimum: min,
+    maximum: max,
+  };
+};
+
+console.log(minMax(numbers));
